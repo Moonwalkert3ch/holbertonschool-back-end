c06bc1141f5d9e6d1945e6a7c386326c2f50e58f
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-19 12:55:54 -0600

    test emp id

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,53 +3,35 @@
 REST API, for a given employee ID, returns information about 
 his/her TODO list progress
 """
+
 import requests
 import sys
 
 
-def main():
-    """ returns information about his/her TODO list progress
-    Args: Param-emp_id - must be an int, ids employee
-    Returns: display data
-    """
-    employee_id = sys.argv[1]
-    response_emp_user = requests.get("https://jsonplaceholder.typicode.com/users")
-    response_emp_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+def employee_todo_progress(employee_id):
+    url = "https://jsonplaceholder.typicode.com/users/{employee_id}"
+    employee_user_url = requests.get(url)
+    empployee_user = employee_user_url.json()
+    employee_name = empployee_user.get("name")
 
-    employee_user = response_emp_user.json()
-    employee_todo = response_emp_todo.json()
-    employee_name = employee_user.get("name")
-    DONE_TASKS = [task.get("title")
-                             for task in employee_todo if task.get("completed") is True]
-    print('Employee {} is done with tasks({}/{}):'.format(employee_name,
-                                                          len(DONE_TASKS),
-                                                          len(TOTAL_NUMBER_OF_TASKS)))
-    print('\n'.join('\t {}'.format(task) for task in tasks))
-
-    #for user in employee_user:
-        #if user["id"] == employee_id:
-            #employee_name = user["name"]
-            #break
-
-    #DONE_TASKS = 0
-    #NUMBER_OF_DONE_TASKS = 0
-
-    #TOTAL_NUMBER_OF_TASKS = []
-
-    #for task in employee_todo:
-        #if task["userId"] == int(employee_id):
-"""
-DONE_TASKS += 1
-if task["completed"] is True:
-NUMBER_OF_DONE_TASKS +=1
-TOTAL_NUMBER_OF_TASKS.append(task["title"])
-
-    print(f"Employee {employee_name} is done with tasks"
-          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    employee_todo_url = requests.get(todo_url, params={"userId": employee_id})
+    employee_todo = employee_todo_url.json()
     
-    for task in DONE_TASKS:
-        print(f"\t{task['title']}")
-        """
-elif:
-    print(f"Error: Failed to retrieve TODO list for employee ID.")
+    TOTAL_NUMBER_OF_TASKS = len(employee_todo)
+    NUMBER_OF_DONE_TASKS = sum(task["completed"] for task in employee_todo)
 
+    print(f"Employee {employee_name} is done with tasks" 
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}) :")
+    print(f"{employee_name}:", end=" ")
+
+    for task in employee_todo:
+        if  task["completed"]:
+            print(f"\n\t{task['title']}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+    employee_id = int(sys.argv[1])
+    employee_todo_progress(employee_id)
