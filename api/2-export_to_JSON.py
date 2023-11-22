9063cfc0afcf34d7dcf908dd8ca81c25beb18655
Moonwalkert3ch <6620@holbertonstudents.com>
2023-11-21 22:50:45 -0600

    export_to_JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python3
-"""export data in the JSON format."""
+"""Export data in JSON format."""
 
 import requests
 import sys
 import json
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
+def export_user_data(user_id):
     url = "https://jsonplaceholder.typicode.com/"
-    emp_user = requests.get(url + "users/{}".format(user_id)).json()
+    
+    # Fetch user details
+    emp_user = requests.get(url + f"users/{user_id}").json()
     username = emp_user.get("username")
+
+    # Fetch user's todos
     emp_todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-with open("{}.json".format(user_id), "w") as f:
-    json.dump({user_id: [{"task": tasks.get("title"),
-                          "completed": tasks.get("completed"),
-                          "username": username} for tasks in 
-                          emp_todos]}, f)
+    # Write data to JSON file
+    with open(f"{user_id}.json", "w") as f:
+        json.dump(
+            {
+                user_id: [
+                    {
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": username
+                    } 
+                    for task in emp_todos
+                ]
+            }, 
+            f,
+            indent=2
+        )
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python 2-export_to_JSON.py <user_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    export_user_data(user_id)
